a332a5db1f61a40c79943428bb3771fc0839e17e
theluffyrossa <luffyrossa@gmail.com>
2026-05-23 15:16:33 -0400

    chore: add Vitest + Playwright + ESLint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'react-hooks', 'react-refresh'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-var': 'error',
+    'eqeqeq': ['error', 'always'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  ignorePatterns: ['dist', 'node_modules', '*.config.*'],
+};
